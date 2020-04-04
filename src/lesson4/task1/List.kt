658746c86a7e39a9e2bf925c70cc36a05ae98e97
romanova <romanova@fom.team>
2020-04-04 21:04:16 +0300

    add decisions

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -221,7 +221,15 @@ fun factorizeToString(n: Int): String {
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var swap = n
+    val result = ArrayList<Int>()
+     while(swap>0) {
+         result.add(swap%base)
+         swap/=base
+     }
+    return result.reversed()
+}
 
 /**
  * Сложная
